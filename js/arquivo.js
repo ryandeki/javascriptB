3bf3afc83734b1078bb8ebbf4189ffc9a4045473
ryandeki <20241ctb0100010@gmail.com>
2025-07-01 12:26:22 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,26 @@
+var global = "variável global"
+console.log(global);
+
+function exemplo2() {
+    var local = "variável local";
+    console.log(local);
+}
+
+exemplo2();
+
+let x = 10;
+if (x > 5) {
+
+    let y = 20;
+    console.log("Valor de y: " + y);
+}
+
+const _constante = "texto";
+
+console.log(console);
+
+function saudacao(){
+    alert("Bem-vindo ao nosso site");
+
+
+}
