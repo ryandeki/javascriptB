0a92ee1bb05b5a3c9f6368c4bc6a63d8da395f80
ryandeki <20241ctb0100010@gmail.com>
2025-07-15 12:36:39 -0300

    Aula dia 15/07

--- exercicios/script.js
+++ exercicios/script.js
@@ -1,23 +1,115 @@
-function somar() {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
+function somarNumeros() {
+    let num1 = Number(document.getElementById("numero1").value);
+    let num2 = Number(document.getElementById("numero2").value);
     document.getElementById("resultado1").innerText = "Resultado: " + (num1 + num2);
 
 }
 
-function verificar() {
+function verificarIdade() {
 
-    let idade = Number(document.getElementaryById("idade").value);
+    let idade = Number(document.getElementById("idade").value);
+    let verificacao;
 
     if (idade >= 18) {
 
-        document.getElementById("resultado2").innerText = "Maior de Idade";
+        verificacao = "Maior de idade";
 
     }
 
     else {
-        document.getElementById("resultado2").innerText = "Memor de Idade";
+        
+        verificacao = "Menor de idade";
+
     }
 
+    document.getElementById("resultado2").innerText = verificacao;
+
+}
+
+function contarCaracteres() {
+
+    let texto = document.getElementById("texto").value;
+    let resultado = texto.length;
+
+    document.getElementById("resultado3").innerText = resultado;
+
+}
+
+function verificarParImpar() {
+
+    let num = Number(document.getElementById("num").value);
+    let verificacao;
+
+    if (num % 2 == 0) {
+
+        verificacao = "É par";
+
+    }
+
+    else {
+
+        verificacao = "É ímpar";
+
+    }
+
+    document.getElementById("resultado4").innerText = verificacao;
+
+}
+
+function transformarCaixaAlta() {
+
+    let nome = document.getElementById("nome").value;
+    let transformar = nome.toUpperCase();
+
+    document.getElementById("resultado5").innerText = transformar;
+
+}
+
+function verificarMaior() {
+
+    let num1 = Number(document.getElementById("n1").value);
+    let num2 = Number(document.getElementById("n2").value);
+    let verificacao;
+
+    if (num1 > num2) {
+
+        verificacao = num1 + " é maior que " + num2;
+
+    }
+
+    else if (num2 > num1) {
+
+        verificacao = num2 + " é maior que " + num1;
+
+    }
+
+    else {
+
+        verificacao = "Os dois números são iguais";
+
+    }
+
+    document.getElementById("resultado6").innerText = verificacao;
+
+}
+
+function gerarTabuada() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let i;
+    let contador;
+
+    for (i = 0; i < 11; i++) {
+
+        contador = num1 + " * " + i;
+        document.getElementById("resultado7").innerText = contador;
+
+    }
+
+    
+
+}
+
+function gerarContador() {
 
 }
